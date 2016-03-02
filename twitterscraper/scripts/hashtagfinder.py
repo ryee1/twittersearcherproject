7e4aa50fb75e462721bbfce8cebed4df3f4120e4
kaylesworth <kevin.aylesworth91@gmail.com>
2016-03-01 18:58:45 -0800

    Update hashtagfinder.py
    Updated with working search results

--- twitterscraper/scripts/hashtagfinder.py
+++ twitterscraper/scripts/hashtagfinder.py
@@ -1,4 +1,20 @@
-def hashtagfinder(word):
-    """Takes in a string, outputs an array/list of hashtags from the twitter API
-    """
-    return [w for w in word]
+from twython import Twython
+
+APP_KEY = ''
+APP_SECRET = ''
+OAUTH_TOKEN = ''
+OAUTH_TOKEN_SECRET = ''
+
+twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+results = twitter.search(q="#warriors", count='100', lang='en', max_id='')
+tweets = []
+for r in results['statuses']:
+	tweets.append(r['text'])
+
+for t in tweets:
+	#Used to print to IDLE in Windows 10, will change later.
+	encodedtweet = t.encode("utf-8", errors='ignore')
+	print(encodedtweet)
+
+
