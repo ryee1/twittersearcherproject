667d1197ebe8776bb2284dbb9f767f07c9a872d6
kaylesworth <kevin.aylesworth91@gmail.com>
2016-03-03 18:53:27 -0800

    Added Tweet class and tweetRetriever

---
+++ twitterscraper/scripts/Tweet.py
@@ -0,0 +1,5 @@
+class Tweet(object):
+	def __init__(self, user, date, tweet):
+		self.user = user
+		self.date = date
+		self.tweet = tweet
--- twitterscraper/scripts/tweetRetreiever.py
+++ twitterscraper/scripts/tweetRetreiever.py
@@ -1,4 +1,5 @@
 from twython import Twython
+from .scripts import Tweet
 
 APP_KEY = '7CnB7vjanuoSYCS1txkery633'
 APP_SECRET = 'kCRmRbS61MENH70dS3eTIkDvgSmqoFXBoOGmtIvlQsfgnbpmdA'
@@ -7,11 +8,11 @@ OAUTH_TOKEN_SECRET = 'UmXwih01oeXKnpW9dTqYO3UkRwZCY1Aj3rki0U6Wbxcvh'
 
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
-def hashtagfinder(word):
-	results = twitter.search(q=word, count='100', lang='en')
+#Uses Twython and Twitter API to grab tweets with the entered word from index
+#Adds username, date, and tweet into a list and returns a list containing all
+def tweetRetriever(word):
+	results = twitter.search(q=word, count='100', lang='en', result_type='recent')
 	tweets = []
 	for r in results['statuses']:
-		tweets.append(r['text'])
-
+		tweets.append([Tweet(r['user']['screen_name'], r['created_at'], r['text'])])
 	return tweets
-
