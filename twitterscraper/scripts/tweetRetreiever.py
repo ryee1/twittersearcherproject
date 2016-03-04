5bf106f4ecea23349806b33874f8e2047c4ccda8
kaylesworth <kevin.aylesworth91@gmail.com>
2016-03-03 18:54:49 -0800

    Update tweetRetreiever.py

--- twitterscraper/scripts/tweetRetreiever.py
+++ twitterscraper/scripts/tweetRetreiever.py
@@ -1,10 +1,10 @@
 from twython import Twython
 from .scripts import Tweet
 
-APP_KEY = '7CnB7vjanuoSYCS1txkery633'
-APP_SECRET = 'kCRmRbS61MENH70dS3eTIkDvgSmqoFXBoOGmtIvlQsfgnbpmdA'
-OAUTH_TOKEN = '218601321-r3zVuvxviq48GhHyJGh2q8un2GTsPdIv3Dn8mi6J'
-OAUTH_TOKEN_SECRET = 'UmXwih01oeXKnpW9dTqYO3UkRwZCY1Aj3rki0U6Wbxcvh'
+APP_KEY = ''
+APP_SECRET = ''
+OAUTH_TOKEN = ''
+OAUTH_TOKEN_SECRET = ''
 
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
