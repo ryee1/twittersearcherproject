2d17ebd5f10251a1449d8639420567777804a277
Richard Yee <rdyyc99@gmail.com>
2016-03-03 20:31:42 -0800

    Fixed detail page

--- twitterscraper/views.py
+++ twitterscraper/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import redirect,render
 def index(request):
     return render(request, 'twitterscraper/index.html')
 
-
 def search(request):
     user_input = request.POST['user_hashtag_input']
     request.session['list_of_tweets'] = tweetRetriever.tweetRetriever(user_input);
@@ -24,5 +23,5 @@ def tableresults(request):
 def details(request):
 	results = request.session['list_of_tweets']
 	user_selected_word = request.POST['user_selected_word']
-	details_list = [result for result in results if user_selected_word in result]
+	details_list = [result['tweet'] for result in results if user_selected_word in result['tweet']]
 	return render(request, 'twitterscraper/details.html', {'details_list':details_list})
