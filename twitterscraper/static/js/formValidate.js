f04772fde3c57c8740c493a812f8fdb25db1d2ee
HarrisonQT <HarrisonQT@Harrisons-MacBook-Pro.local>
2016-03-07 17:11:20 -0800

    added form validation and fixed header and index forms

---
+++ twitterscraper/static/js/formValidate.js
@@ -0,0 +1,7 @@
+function validateForm() {
+    var x = document.forms["searchForm"]["user_hashtag_input"].value;
+    if (!(!/[~`!#$%\^&*+=\-\[\]\\';,/{ }|\\":<>\.?]/g.test(x))) {
+        alert("Name must be filled out");
+        return false;
+    }
+} 
