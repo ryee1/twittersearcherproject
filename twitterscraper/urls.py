1b76e1ef4fc7339b57cc6a65cea99f14434e6f4e
kaylesworth <kevin.aylesworth91@gmail.com>
2016-03-08 18:29:58 -0800

    Update urls.py
    Added count page

--- twitterscraper/urls.py
+++ twitterscraper/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^results/$', views.results, name='results'),
     url(r'^tableresults/$', views.tableresults, name='tableresults'),
     url(r'^details/$', views.details, name='details'),
+    url(r'^count/$', views.count, name='count'),
 ]
