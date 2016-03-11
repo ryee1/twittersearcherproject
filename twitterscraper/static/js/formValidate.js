2221d84fc656994eb141b801914eab48f27ec5f0
kaylesworth <kevin.aylesworth91@gmail.com>
2016-03-10 18:00:01 -0800

    Update formValidate.js

--- twitterscraper/static/js/formValidate.js
+++ twitterscraper/static/js/formValidate.js
@@ -1,6 +1,6 @@
 function validateForm() {
     var x = document.forms["searchForm"]["user_hashtag_input"].value;
-    if (!(!/[~`!#$%\^&*+=\-\[\]\\';,/{ }|\\":<>\.?]/g.test(x))) {
+    if (!(!/[~`!$%\^&*+=\-\[\]\\';,/{ }|\\":<>\.?]/g.test(x))) {
         alert("Name must be filled out");
         return false;
     }
